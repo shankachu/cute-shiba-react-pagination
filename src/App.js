76cfd40d990a2917f603a7db39e4a1f2600ef682
s.cao <shanshan.cao@thalesdigital.io>
2022-07-22 17:57:10 +0200

    Got some dogs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-
-const Dog = (props) => {
-  return (
-    <>
-      <h1>Breed: {props.breeds}</h1>
-      <h2>Height: {props.height}</h2>
-    </>
-  )
-}
-
-const Dog1 = {
-  "breeds": [],
-  "height": 352,
-  "id": "GLgO8VAmO",
-  "url": "https://cdn2.thedogapi.com/images/GLgO8VAmO.png",
-  "width": 329
-}
+import DogCard from './DogCard';
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `https://api.thedogapi.com/v1/images/search?api_key=${API_KEY}&limit=20&order=Desc`;
+  const [dogs, setDogs]= useState([]);
+
   const fetchDogs = async () => {
-    const response = await fetch(url)
+    const response = await fetch(url, {});
     const data = await response.json();
-    console.log(data);
-  }
+    setDogs(data);
+  };
 
   useEffect(() => {
-    // Page load 100 dogs when the page starts
     fetchDogs();
   }, []);
 
@@ -42,8 +27,7 @@ function App() {
         </div>
       </header>
       <div className="container">
-        <Dog className="card"/>
-        <img src={ Dog1.url !== 'N/A' ? Dog1.url : 'https://via.placeholder.com/400'} alt="" />
+        {dogs.map(dog => <DogCard dog={dog} />)}
       </div>
     </div>
   );
